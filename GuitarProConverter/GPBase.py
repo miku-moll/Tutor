be0faa761ad1f4050f5e667a6bd20a4ef5fb5344
佐政 李 <lizuozheng@live.com>
2020-04-01 20:23:08 +0800

    第二次

--- GuitarProConverter/GPBase.py
+++ GuitarProConverter/GPBase.py
@@ -1,5 +1,9 @@
 from enum import Enum,unique
 
+
+#=============================
+#枚举类
+
 @unique
 class BeatStatus(Enum):
     empty = 0
@@ -12,9 +16,11 @@ class BeatStrokeDirection(Enum):
     up = 1
     down = 2
 
-#推弦技巧
+
 @unique
 class BendType(Enum):
+    '''推弦技巧'''
+
     # 无预设
     none = 0
     # 预设推弦类型
@@ -37,7 +43,7 @@ class BendType(Enum):
     releaseUp = 8
     invertedDip = 9
     return_ = 10
-    releaseDown = 1
+    releaseDown = 11
 
 @unique
 class ChordType(Enum):
@@ -204,36 +210,51 @@ class SimileMark(Enum):
     firstOfDouble = 2
     secondOfDouble = 3
 
+@unique
+class WahState(Enum):
+    off =-2
+    none=-1
+    opened = 0
+    closed = 100
+#===========================
+
+class WahEffect:
+
+    state = WahState.none
+    display = False
+
+
+#===============================================
 
 #抽象类
 class HarmonicEffect:
     fret = 0
     type = 0
 
-#派生
+#及其派生
 class NaturalHarmonic(HarmonicEffect):
     def NaturalHarmonic(self): type = 1
 
 class ArtificialHarmonic(HarmonicEffect):
-    PitchClass pitch;
+    pitch = 0
     octave = Octave.none
-    def ArtificialHarmonic(self, pitch = null, octave = Octave.none):
-        self.pitch = pitch;
-        self.octave = octave;
-        self.type = 2;
+    def ArtificialHarmonic(self, pitch = 0, octave = Octave.none):
+        self.pitch = pitch; self.octave = octave; self.type = 2
 
 class TappedHarmonic(HarmonicEffect):
     def TappedHarmonic(self, fret = 0):
-        self.fret = fret;
-        self.type = 3
+        self.fret = fret; self.type = 3
 
 class PinchHarmonic(HarmonicEffect):
-    def PinchHarmonic(self): type = 4
+    def __init__(self): type = 4
+
 class SemiHarmonic(HarmonicEffect):
-    def SemiHarmonic(self): type = 5
+    def __init__(self): type = 5
+
 class FeedbackHarmonic(HarmonicEffect):
    def FeedbackHarmonic(self): type = 6
 
+#=======================================================
 
 class LyricLine:
     startingMeasure = 1
@@ -244,7 +265,7 @@ class Lyrics:
     __maxLineCount = 5
     trackChoice = -1
 
-    def Lyrics(self):
+    def __init__(self):
         trackChoice = -1
         lines = LyricLine[__maxLineCount]
         x = 0
@@ -252,15 +273,79 @@ class Lyrics:
             lines[x] = LyricLine()
             x += 1
 
-
-
-
+#==================================================
 class Barre:
     start = 0
     end = 0
     fret = 0
-    def Barre(self, fret=0, start=0, end=0):
+    def Barre(self, fret, start, end):
         self.start = start; self.fret = fret; self.end = end
     
     def range(self):   
         return [self.start, self.end]
+
+class MidiChannel:
+
+    channel=effectChannel=instrument=volume=balance=chorus=reverb=phaser=tremolo=bank = 0
+
+    DEFAULT_PERCUSSION_CHANNEL = 9
+
+    def __init__(self):
+        self.channel = 0; self.effectChannel = 1; self.instrument = 25; self.volume = 104
+        self.balance = 64; self.chorus = 0; self.reverb = 0; self.phaser = 0
+        self.tremolo = 0; self.bank = 0
+
+    def isPercussionChannel(self):
+        return channel % 16 == DEFAULT_PERCUSSION_CHANNEL
+
+
+class MixTableItem:
+    '''
+    Attributes:
+        int value
+        int duration
+        bool allTracks
+        bool isNull
+    '''
+    
+    def __init__(self, value = 0, duration = 0, allTracks = False, isNull = True):
+        self.value = value; self.duration = duration; self.allTracks = allTracks; self.isNull = isNull
+
+class MixTableChange:
+    '''
+    Attributes:
+        string tempoName
+        bool hideTempo
+        bool useRSE
+        MixTableItem instrument,volume,balance,chorus,reverb,phaser,tremolo,tempo,wah,rse
+    '''
+    instrument = MixTableItem()
+    volume = MixTableItem()
+    balance = MixTableItem()
+    chorus = MixTableItem()
+    reverb = MixTableItem()
+    phaser = MixTableItem()
+    tremolo = MixTableItem()
+    tempo = MixTableItem()
+    wah = MixTableItem()
+    #rse = RSEInstrument()
+
+    def __init__(self, tempoName = "", hideTempo = True, useRSE = True):
+    
+        self.tempoName = tempoName;
+        self.hideTempo = hideTempo;
+        self.useRSE = useRSE;
+
+    def isJustWah(self):
+    
+        return (instrument.isNull == True and volume.isNull == True and balance.isNull == True and
+               chorus.isNull == True and reverb.isNull == True and phaser.isNull == True and 
+               tremolo.isNull == True and wah.isNull == False)
+    
+
+class myColor:
+    def __init__(self, r=1.0, g=1.0, b=1.0, a = 255.0):
+        this.r = r / 255.0;
+        this.g = g /255.0;
+        this.b = b / 255.0;
+        this.a = a / 255.0;
